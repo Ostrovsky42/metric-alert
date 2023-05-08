c3a562f42f944d19ca36ea356d19a4384dfd267c
Sergey Ostrovskiy <s.ostrovskiy@4payments.com>
2023-05-08 04:00:40 +0300

    added agent_test & close resp body

--- internal/agent/agent_test.go
+++ internal/agent/agent_test.go
@@ -9,18 +9,14 @@ import (
 )
 
 func TestSendMetric(t *testing.T) {
-	// Setup test data
 	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Verify request URL and method
 		if r.URL.Path != "/update/gauge/metric/1" || r.Method != "POST" {
 			t.Errorf("Unexpected request URL or method: %v %v", r.URL.Path, r.Method)
 		}
-		// Verify request header
 		contentType := r.Header.Get("Content-Type")
 		if contentType != "text/plain" {
 			t.Errorf("Unexpected content type: %v", contentType)
 		}
-		// Write a response
 		fmt.Fprint(w, "OK")
 	}))
 	defer testServer.Close()
