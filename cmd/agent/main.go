bae4d94c0ef002a051e8b607bffefcd4d67b4d67
Sergey Ostrovskiy <s.ostrovskiy@4payments.com>
2023-05-08 03:18:16 +0300

    iteration 2

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,22 @@
 package main
 
-func main() {}
+import (
+	"metric-alert/internal/agent"
+	"net/http"
+	"time"
+)
+
+func main() {
+	pollInterval := 2 * time.Second
+	reportInterval := 10 * time.Second
+	serverURL := "http://localhost:8080"
+
+	a := agent.NewAgent(reportInterval, pollInterval, serverURL)
+	a.Run()
+
+	mux := http.NewServeMux()
+	err := http.ListenAndServe(`:8081`, mux)
+	if err != nil {
+		panic(err)
+	}
+}
