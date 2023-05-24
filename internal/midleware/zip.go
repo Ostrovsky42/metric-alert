3455bc6aaa12aea907821d452f4c5c0b8011ae82
Sergey Ostrovskiy <s.ostrovskiy@4payments.com>
2023-05-25 02:27:33 +0300

    iter8

--- internal/midleware/zip.go
+++ internal/midleware/zip.go
@@ -11,36 +11,43 @@ import (
 type ZipMiddleware struct {
 	zContentTypes []string
 	log           zerolog.Logger
-	gz            *gzip.Writer
+	gzipW         *gzip.Writer
 }
 
 func NewZipMiddleware(log zerolog.Logger, level int) ZipMiddleware {
-	gz, err := gzip.NewWriterLevel(nil, level)
+	gzW, err := gzip.NewWriterLevel(nil, level)
 	if err != nil {
 		log.Fatal().Err(err).Msg("err create gzip writer")
 	}
 
 	return ZipMiddleware{
-		zContentTypes: []string{"text/html", "application/json"},
-		log:           log,
-		gz:            gz,
+		log:   log,
+		gzipW: gzW,
+		zContentTypes: []string{
+			"application/json",
+			"text/html",
+		},
 	}
 }
 
 type gzipWriter struct {
 	http.ResponseWriter
-	Writer io.Writer
+	gzipW *gzip.Writer
 }
 
-func (w gzipWriter) Write(b []byte) (int, error) {
-	return w.Writer.Write(b)
+func (w *gzipWriter) Write(b []byte) (int, error) {
+	return w.gzipW.Write(b)
 }
 
-func (z ZipMiddleware) Zip(next http.Handler) http.Handler {
+func (z *ZipMiddleware) Zip(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if z.isNeedZipped(r) {
-			z.gz.Reset(w)
-			next.ServeHTTP(&gzipWriter{ResponseWriter: w, Writer: z.gz}, r)
+			defer z.gzipW.Close()
+
+			w.Header().Set("Content-Encoding", "gzip")
+			z.gzipW.Reset(w)
+
+			next.ServeHTTP(&gzipWriter{ResponseWriter: w, gzipW: z.gzipW}, r)
 
 			return
 		}
@@ -49,7 +56,7 @@ func (z ZipMiddleware) Zip(next http.Handler) http.Handler {
 	})
 }
 
-func (z ZipMiddleware) UnZip(next http.Handler) http.Handler {
+func (z *ZipMiddleware) UnZip(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if !strings.Contains(r.Header.Get("Content-Encoding"), "gzip") {
 			next.ServeHTTP(w, r)
@@ -72,12 +79,11 @@ func (z ZipMiddleware) UnZip(next http.Handler) http.Handler {
 	})
 }
 
-func (z ZipMiddleware) isNeedZipped(r *http.Request) bool {
+func (z *ZipMiddleware) isNeedZipped(r *http.Request) bool {
 	if strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
 		reqContentType := r.Header.Get("Content-Type")
 		for _, contentType := range z.zContentTypes {
 			if contentType == reqContentType {
-
 				return true
 			}
 		}
