3391ad3eddc47778feb682dd1750f9e99798c716
Sergey Ostrovskiy <s.ostrovskiy@4payments.com>
2023-06-09 14:07:26 +0300

    iter12

--- internal/handlers/update.go
+++ internal/handlers/update.go
@@ -68,6 +68,16 @@ func (m MetricAlerts) UpdateMetricWithBody(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
+	getmetric, err := m.metricStorage.GetMetric(metric.ID)
+	if err != nil {
+		logger.Log.Error().Err(err).Msg("error set metric")
+		w.WriteHeader(http.StatusInternalServerError)
+
+		return
+	}
+
+	logger.Log.Debug().Interface("getmetric", getmetric).Interface("seted_metrics", metric).Send()
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(data)
@@ -102,14 +112,22 @@ func (m MetricAlerts) UpdateMetricsWithBody(w http.ResponseWriter, r *http.Reque
 
 		return
 	}
+	getMetrics, err := m.metricStorage.GetAllMetric()
+	if err != nil {
+		logger.Log.Error().Err(err).Msg("error set metric")
+		w.WriteHeader(http.StatusInternalServerError)
 
-	data, err := json.Marshal(metrics)
+		return
+	}
+
+	data, err := json.Marshal(getMetrics)
 	if err != nil {
 		logger.Log.Error().Err(err).Msg("err encode data")
 		w.WriteHeader(http.StatusInternalServerError)
 
 		return
 	}
+	logger.Log.Debug().Interface("all_metrics", data).Interface("seted_metrics", metrics).Send()
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
