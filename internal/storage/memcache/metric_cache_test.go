89ac2acdf7c128e8e015b0bc4691f482e0ed5ec9
Sergey Ostrovskiy <s.ostrovskiy@4payments.com>
2023-06-14 03:14:03 +0300

    iter13 fix memCache test

--- internal/storage/memcache/metric_cache_test.go
+++ internal/storage/memcache/metric_cache_test.go
@@ -86,53 +86,59 @@ func TestMemStorage_GetMetric(t *testing.T) {
 
 func TestMemStorage_SetMetric(t *testing.T) {
 	tests := []struct {
-		name    string
-		storage *MemCache
-		metric  entities.Metrics
-		want    *entities.Metrics
-		err     error
+		name     string
+		storage  *MemCache
+		metric   entities.Metrics
+		metricID string
+		want     *entities.Metrics
+		err      error
 	}{
 		{
-			name:    "positive test",
-			storage: &MemCache{storage: map[string]entities.Metrics{}},
-			metric:  entities.Metrics{ID: "metric", MType: entities.Gauge, Value: floatPointer(44)},
-			want:    &entities.Metrics{ID: "metric", MType: entities.Gauge, Value: floatPointer(44)},
-			err:     nil,
+			name:     "positive test",
+			storage:  &MemCache{storage: map[string]entities.Metrics{}},
+			metric:   entities.Metrics{ID: "metric", MType: entities.Gauge, Value: floatPointer(44)},
+			metricID: "metric",
+			want:     &entities.Metrics{ID: "metric", MType: entities.Gauge, Value: floatPointer(44)},
+			err:      nil,
 		},
 		{
-			name:    "positive test",
-			storage: &MemCache{storage: map[string]entities.Metrics{}},
-			metric:  entities.Metrics{ID: "metric", MType: entities.Counter, Delta: intPointer(55)},
-			want:    &entities.Metrics{ID: "metric", MType: entities.Counter, Delta: intPointer(55)},
-			err:     nil,
+			name:     "positive test",
+			storage:  &MemCache{storage: map[string]entities.Metrics{}},
+			metric:   entities.Metrics{ID: "metric", MType: entities.Counter, Delta: intPointer(55)},
+			metricID: "metric",
+			want:     &entities.Metrics{ID: "metric", MType: entities.Counter, Delta: intPointer(55)},
+			err:      nil,
 		},
 		{
-			name:    "negative test",
-			storage: &MemCache{storage: map[string]entities.Metrics{}},
-			metric:  entities.Metrics{ID: "metric", MType: entities.Counter, Delta: intPointer(55)},
-			want:    nil,
-			err:     errorNoFound,
+			name:     "negative test",
+			storage:  &MemCache{storage: map[string]entities.Metrics{}},
+			metric:   entities.Metrics{ID: "metric", MType: entities.Counter, Delta: intPointer(55)},
+			metricID: "ne metric",
+			want:     nil,
+			err:      errorNoFound,
 		},
 		{
-			name:    "negative test",
-			storage: &MemCache{storage: map[string]entities.Metrics{}},
-			metric:  entities.Metrics{ID: "metric", MType: entities.Counter, Delta: intPointer(155)},
-			want:    nil,
-			err:     errorNoFound,
+			name:     "negative test",
+			storage:  &MemCache{storage: map[string]entities.Metrics{}},
+			metric:   entities.Metrics{ID: "metric", MType: entities.Counter, Delta: intPointer(155)},
+			metricID: "ne metric",
+			want:     nil,
+			err:      errorNoFound,
 		},
 		{
-			name:    "positive empty name key?",
-			storage: &MemCache{storage: map[string]entities.Metrics{}},
-			metric:  entities.Metrics{ID: "", MType: entities.Gauge, Value: floatPointer(1)},
-			want:    &entities.Metrics{ID: "", MType: entities.Gauge, Value: floatPointer(1)},
-			err:     nil,
+			name:     "positive empty name key?",
+			storage:  &MemCache{storage: map[string]entities.Metrics{}},
+			metric:   entities.Metrics{ID: "", MType: entities.Gauge, Value: floatPointer(1)},
+			metricID: "",
+			want:     &entities.Metrics{ID: "", MType: entities.Gauge, Value: floatPointer(1)},
+			err:      nil,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 
 			tt.storage.SetMetric(context.Background(), tt.metric)
-			got, got1 := tt.storage.GetMetric(context.Background(), tt.metric.ID)
+			got, got1 := tt.storage.GetMetric(context.Background(), tt.metricID)
 			assert.Equal(t, got, tt.want)
 			assert.Equal(t, got1, tt.err)
 		})
