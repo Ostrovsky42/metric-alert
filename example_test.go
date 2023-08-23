a6f9dcd223e55daef1fd9678e0f8c80bdd80a159
Sergey Ostrovskiy <s.ostrovskiy@4payments.com>
2023-08-23 11:12:11 +0300

    iter18 fix example_test.go

--- example_test.go
+++ example_test.go
@@ -4,106 +4,88 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+
 	"github.com/go-chi/chi"
-	"github.com/stretchr/testify/assert"
+
 	"metric-alert/internal/server/entities"
 	"metric-alert/internal/server/handlers"
 	"metric-alert/internal/server/repository"
-	"net/http"
-	"net/http/httptest"
-	"testing"
 )
 
-func ExampleUpdateMetricWithBody() {
-	_ = func(t *testing.T) {
-		mockStorage, _ := repository.InitRepo("", "", 40, false)
+func ExampleMetricAlerts_UpdateMetricWithBody() {
+	mockStorage, _ := repository.InitRepo("", "", 40, false)
 
-		var value float64 = 5
-		testMetric := entities.Metrics{
+	var value float64 = 5
+	testMetric := entities.Metrics{
+		MType: entities.Gauge,
+		ID:    "test_gauge",
+		Value: &value,
+	}
+	data, _ := json.Marshal(testMetric)
+
+	req := httptest.NewRequest(http.MethodPost, "/update/", bytes.NewReader(data))
+
+	r := chi.NewRouter()
+
+	metricAlerts := handlers.NewMetric(mockStorage, nil)
+	r.Post("/update/", metricAlerts.UpdateMetricWithBody)
+
+	rr := httptest.NewRecorder()
+	r.ServeHTTP(rr, req)
+}
+
+func ExampleMetricAlerts_UpdateMetricsWithBody() {
+	mockStorage, _ := repository.InitRepo("", "", 40, false)
+
+	var value float64 = 5
+	var valueCount int64 = 5
+	testMetric := []entities.Metrics{
+		{
 			MType: entities.Gauge,
 			ID:    "test_gauge",
 			Value: &value,
-		}
-		data, _ := json.Marshal(testMetric)
-
-		req := httptest.NewRequest(http.MethodPost, "/update/", bytes.NewReader(data))
-
-		r := chi.NewRouter()
-
-		metricAlerts := handlers.NewMetric(mockStorage, nil)
-		r.Post("/update/", metricAlerts.UpdateMetricWithBody)
-
-		rr := httptest.NewRecorder()
-		r.ServeHTTP(rr, req)
-
-		assert.Equal(t, http.StatusOK, rr.Code)
-
-		finelyMetric, err := mockStorage.GetMetric(context.Background(), testMetric.ID)
-		assert.Equal(t, &testMetric, finelyMetric)
-		assert.Equal(t, nil, err)
+		},
+		{
+			MType: entities.Counter,
+			ID:    "test_counter",
+			Delta: &valueCount,
+		},
 	}
+	data, _ := json.Marshal(testMetric)
+
+	req := httptest.NewRequest("POST", "/updates/", bytes.NewReader(data))
+
+	r := chi.NewRouter()
+
+	metricAlerts := handlers.NewMetric(mockStorage, nil)
+	r.Post("/updates/", metricAlerts.UpdateMetricsWithBody)
+
+	rr := httptest.NewRecorder()
+	r.ServeHTTP(rr, req)
 }
 
-func ExampleUpdateMetricsWithBody() {
-	_ = func(t *testing.T) {
-		mockStorage, _ := repository.InitRepo("", "", 40, false)
+func ExampleMetricAlerts_GetValueWithBody() {
+	mockStorage, _ := repository.InitRepo("", "", 40, false)
 
-		var value float64 = 5
-		testMetric := []entities.Metrics{
-			{
-				MType: entities.Gauge,
-				ID:    "test_gauge",
-				Value: &value,
-			},
-			{
-				MType: entities.Counter,
-				ID:    "test_counter",
-			},
-		}
-		data, _ := json.Marshal(testMetric)
-
-		req := httptest.NewRequest(http.MethodPost, "/updates/", bytes.NewReader(data))
-
-		r := chi.NewRouter()
-
-		metricAlerts := handlers.NewMetric(mockStorage, nil)
-		r.Post("/updates/", metricAlerts.UpdateMetricsWithBody)
-
-		rr := httptest.NewRecorder()
-		r.ServeHTTP(rr, req)
-
-		assert.Equal(t, http.StatusBadRequest, rr.Code)
+	var value float64 = 5
+	testMetric := entities.Metrics{
+		MType: entities.Gauge,
+		ID:    "test_gauge",
+		Value: &value,
 	}
-}
 
-func ExampleGetValueWithBody() {
-	_ = func(t *testing.T) {
-		mockStorage, _ := repository.InitRepo("", "", 40, false)
+	mockStorage.SetMetric(context.Background(), testMetric)
+	data, _ := json.Marshal(testMetric)
 
-		var value float64 = 5
-		testMetric := entities.Metrics{
-			MType: entities.Gauge,
-			ID:    "test_gauge",
-			Value: &value,
-		}
+	req := httptest.NewRequest(http.MethodGet, "/value/", bytes.NewReader(data))
 
-		mockStorage.SetMetric(context.Background(), testMetric)
-		data, _ := json.Marshal(testMetric)
+	r := chi.NewRouter()
 
-		req := httptest.NewRequest(http.MethodGet, "/value/", bytes.NewReader(data))
+	metricAlerts := handlers.NewMetric(mockStorage, nil)
+	r.Post("/value/", metricAlerts.GetValueWithBody)
 
-		r := chi.NewRouter()
-
-		metricAlerts := handlers.NewMetric(mockStorage, nil)
-		r.Post("/value/", metricAlerts.GetValueWithBody)
-
-		rr := httptest.NewRecorder()
-		r.ServeHTTP(rr, req)
-
-		assert.Equal(t, http.StatusOK, rr.Code)
-
-		finelyMetric, err := mockStorage.GetMetric(context.Background(), testMetric.ID)
-		assert.Equal(t, &testMetric, finelyMetric)
-		assert.Equal(t, nil, err)
-	}
+	rr := httptest.NewRecorder()
+	r.ServeHTTP(rr, req)
 }
